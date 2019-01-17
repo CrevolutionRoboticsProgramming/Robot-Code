7cb32a69cd1cf7c397ed8e0d718ff1af36cb6e5a
Jason Deal <jasond@dde-inc.com>
2019-01-17 10:59:14 -0500

    updated constants

--- Robot-Code-2019/src/main/java/org/frc2851/robot/Constants.java
+++ Robot-Code-2019/src/main/java/org/frc2851/robot/Constants.java
@@ -10,12 +10,23 @@ public class Constants
 
     private Constants() { }
 
-    public final int talonLeftA = 0;
-    public final int talonLeftB = 0;
-    public final int talonLeftC = 0;
-    public final int talonRightA = 0;
-    public final int talonRightB = 0;
-    public final int talonRightC = 0;
+    // Drivetrain
+    public final int leftDriveMaster = 1;
+    public final int leftDriveSlaveA = 2;
+    public final int leftDriveSlaveB = 3;
+    public final int rightDriveMaster = 4;
+    public final int rightDriveSlaveA = 5;
+    public final int rightDriveSlaveB = 6;
+
+    public final int pigeonID = 42;
+
+    // Elevator
+    public final int elevatorMaster = 7;
+    public final int elevatorSlave = 8;
+
+    // Climber
+    public final int gorillaMaster = 9;
+    public final int gorillaSlave = 10;
 
     public final int talonTimeout = 20;
 }
