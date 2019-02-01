b747c7c4f39e9d365265a2c38c770f1ad46f8ab5
Samantha Neveau <NeveauS@stu.uticak12.org>
2019-02-01 18:07:03 -0500

    Start work on intake

--- Robot-Code-2019/src/main/java/org/frc2851/robot/subsystems/Intake.java
+++ Robot-Code-2019/src/main/java/org/frc2851/robot/subsystems/Intake.java
@@ -1,4 +1,63 @@
 package org.frc2851.robot.subsystems;
 
-public class Intake {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import org.frc2851.crevolib.subsystem.Command;
+import org.frc2851.crevolib.subsystem.Subsystem;
+
+public class Intake extends Subsystem {
+
+    public Intake(){
+        super("Intake");
+    }
+
+    @Override
+    protected boolean init(){
+
+    }
+
+    public boolean numatic(){
+        TalonSRX talon1 = new TalonSRX(0);
+        if(){
+            talon1.set(ControlMode.PercentOutput, .25);
+        }
+    }
+
+    public boolean motorIntake(){
+
+    }
+
+    public boolean motorOutake(){
+
+    }
+
+    @Override
+    public Command getTeleopCommand(){
+        return new Command() {
+            @Override
+            public String getName() {
+                return "default";
+            }
+
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean init() {
+                return true;
+            }
+
+            @Override
+            public void update() {
+
+            }
+
+            @Override
+            public void stop() {
+
+            }
+        }
+    }
 }
