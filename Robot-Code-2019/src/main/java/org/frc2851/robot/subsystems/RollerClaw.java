e03f68b5a5c62f88cd16b07b8b311766871c2c0f
Parker-Crevo2851 <Parkertheparker@gmail.com>
2019-02-02 13:15:38 -0500

    I did all the motor stuff

--- Robot-Code-2019/src/main/java/org/frc2851/robot/subsystems/RollerClaw.java
+++ Robot-Code-2019/src/main/java/org/frc2851/robot/subsystems/RollerClaw.java
@@ -2,23 +2,31 @@ package org.frc2851.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import org.frc2851.crevolib.drivers.TalonSRXFactory;
+import org.frc2851.crevolib.io.Button;
+import org.frc2851.crevolib.io.Controller;
 import org.frc2851.crevolib.subsystem.Command;
 import org.frc2851.crevolib.subsystem.Subsystem;
 import org.frc2851.robot.Constants;
+import org.frc2851.robot.Robot;
 
 public class RollerClaw extends Subsystem {
+    private Controller mController = Robot.operator;
+
+    private Button.ButtonID roller = Button.ButtonID.B;
     private WPI_TalonSRX _Motor;
     RollerClaw mInstance = new RollerClaw();
     Constants mConstants = Constants.getInstance();
 
     private RollerClaw() {
-        super("RollerClaw")
+        super("RollerClaw");
     }
 
     @Override
     protected boolean init() {
 
         _Motor = TalonSRXFactory.createDefaultMasterWPI_TalonSRX(mConstants.rollerClawTalon);
+        mController.config(roller, Button.ButtonMode.RAW);
+
         return true;
     }
 
@@ -39,11 +47,16 @@ public class RollerClaw extends Subsystem {
 
             @Override
             public boolean init() {
-                return false;
+              return false;
             }
 
             @Override
             public void update() {
+                if (mController.get(roller)) {
+                    _Motor.set(.5);
+                } else {
+                    _Motor.set(0);
+                }
 
             }
 
