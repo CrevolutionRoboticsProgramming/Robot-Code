a2a4e0d1b0d06a2f080297f51593fe37b7e536a3
RyanBoring <boring.ryan.alan@gmail.com>
2019-02-02 15:20:23 -0500

    Clean up

--- Robot-Code-2019/src/main/java/org/frc2851/robot/subsystems/Hatcher.java
+++ Robot-Code-2019/src/main/java/org/frc2851/robot/subsystems/Hatcher.java
@@ -1,6 +1,6 @@
 package org.frc2851.robot.subsystems;
 
-import badlog.lib.BadLog;//Bad Log,bad you are third party so that is very bad.
+import badlog.lib.BadLog;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import org.frc2851.crevolib.io.Button;
 import org.frc2851.crevolib.io.Controller;
@@ -13,7 +13,7 @@ public class Hatcher extends Subsystem {
 
     private Button.ButtonID hatcherExtend = Button.ButtonID.RIGHT_BUMPER;
     private Button.ButtonID hatcherActuate = Button.ButtonID.LEFT_BUMPER;
-    //I got rid of the extra buttons so that one extends and one actuates
+
     private DoubleSolenoid mExtendSol, mActuateSol;
     private Controller mController = Robot.operator;
     private Constants mConstants = Constants.getInstance();
@@ -38,11 +38,11 @@ public class Hatcher extends Subsystem {
         mExtendSol = new DoubleSolenoid(mConstants.pcmID, mConstants.extendHatcherForward, mConstants.extendHatcherReverse);
         mActuateSol = new DoubleSolenoid(mConstants.pcmID, mConstants.actuateHatcherForward, mConstants.actuateHatcherReverse);
 
+        mController.config(hatcherExtend, Button.ButtonMode.RAW);
+        mController.config(hatcherActuate, Button.ButtonMode.TOGGLE);
+
         reset();
 
-        mController.config(hatcherExtend, Button.ButtonMode.TOGGLE);
-        mController.config(hatcherActuate, Button.ButtonMode.TOGGLE);
-        // I changed it from RAW to TOGGLE
         return true;
     }
 
@@ -62,11 +62,11 @@ public class Hatcher extends Subsystem {
 
             @Override
             public boolean init() {
-                reset(); //Jason isn't sure that this is necessary "Worth the lost cycles of uncertainty"
+                reset();
 
-                BadLog.createTopic("HatcherActuated", BadLog.UNITLESS, () -> mActuateSol.get() == DoubleSolenoid.Value.kReverse ? 1.0 : 0.0, "hide", "join:Hatcher Actuated ");
-                BadLog.createTopic("HatcherExtended", BadLog.UNITLESS, () -> mExtendSol.get() == DoubleSolenoid.Value.kForward ? 1.0 : 0.0, "hide", "join:Hatcher Extended  ");
-                //I added Badlog.
+                BadLog.createTopic("Hatcher Actuated", BadLog.UNITLESS, () -> mActuateSol.get() == DoubleSolenoid.Value.kReverse ? 1.0 : 0.0, "hide", "join:Hatcher Actuated ");
+                BadLog.createTopic("Hatcher Extended", BadLog.UNITLESS, () -> mExtendSol.get() == DoubleSolenoid.Value.kForward ? 1.0 : 0.0, "hide", "join:Hatcher Extended  ");
+
                 return true;
             }
 
@@ -83,7 +83,6 @@ public class Hatcher extends Subsystem {
                 } else {
                     mActuateSol.set(DoubleSolenoid.Value.kForward);
                 }
-                // I got rid of the else if statements that were no longer needed since it was changed from RAW to TOGGLE.
             }
 
             @Override
