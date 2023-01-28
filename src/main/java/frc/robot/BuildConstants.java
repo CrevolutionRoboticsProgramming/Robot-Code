daf95095ea885e7572ca226982a4ea9c5cc28e4b
jmdeal <jasond@dde-inc.com>
2023-01-28 17:08:27 -0500

    feat: update build system to 2023

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot-Code-2019";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 306;
+  public static final String GIT_SHA = "be8fa69be8df081a81d0ff39087bbd17481831b7";
+  public static final String GIT_DATE = "2023-01-28 16:12:57 EST";
+  public static final String GIT_BRANCH = "2023-update";
+  public static final String BUILD_DATE = "2023-01-28 17:05:31 EST";
+  public static final long BUILD_UNIX_TIME = 1674943531215L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
